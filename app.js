0c17ef3a8c77e9d9686e6af3ee81cf2948205f89
Gianluca Esposito <giaespo@gmail.com>
2016-07-28 22:18:11 +0200

    added a general event assert when click, use attr gotopage to manage url change

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 var defaultRoute = {
 		 "route":{
-		      left:[{type:"route",url:V("url")},{type:'users',ref:V("url"),users:V("params")},{type:"template",path:V("path"),id:V("id")}],
+		      left:[{type:"route",url:V("url")},{type:'params',ref:V("url"),data:V("params")},{type:"template",ref:V("url"),path:V("path"),id:V("id")}],
               right:[MostroRoute.defaultAction()],
               debug : []
-	      }
+	      },
 	 }
 
 // change default route rule	 
-MostroRoute.changeDefaultRouting(defaultRoute);
+//MostroRoute.changeDefaultRouting(defaultRoute);
 //start routing, this register route rules in Engine
 MostroRoute.startRouting();
 // start Engine
 Engine.start();
 
-//some fact asserted as example
-Engine.assertFact({type:"partial",name:"header",path:"header"});
-Engine.assertFact({type:"partial",name:"footer",path:"footer"});
 
-Engine.assertFact({type:'users', ref:"users",users:[{name:'gian',surname:'espo'},{name:'pippo',surname:'pino'},{name:'gino',surname:'giorgio'}]})
-Engine.assertFact({type:"template",path:'users',id:'users'})
-Engine.assertFact({type:"route",url:'users'});
+function eventAssert(e) {
+	e.preventDefault();
+	page = $(e.target).attr('gotopage');
+	if (page){
+		Engine.assertFact({type:"default", target:page,scope:page});
+		//Engine.assertFact({type:"route",url:page});
+	}else{
+        Engine.assertFact({type:"event", event:e.type,targetId:e.target.id});
+    }
+}
+
+$(document).bind("click", eventAssert);
+
+//some fact asserted as example
+Engine.assertFact({type:"partial",name:"header",path:"header",scope:"global"});
+Engine.assertFact({type:"partial",name:"footer",path:"footer",scope:"global"});
+
+Engine.assertFact({type:'params', ref:"login",params:[],scope:"login"});
+Engine.assertFact({type:"template",path:'login',id:'main',ref:"login",scope:"login"});
+Engine.assertFact({type:"route",url:'login',scope:"login"});
